9a3056972eb5e824754149b9c0e84daaa6ee5888
ArturMingl <adiscov90@gmail.com>
2023-08-06 08:33:03 +0300

    Toy added

---
+++ src/Toy.java
@@ -0,0 +1,43 @@
+public class Toy {
+    private int id;           // id игрушки
+    private String name;      // название игрушки
+    private int quantity;     // количество
+    private double weight;    // частота выпадения
+
+    public Toy(int id, String name, int quantity, double weight) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Toy{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", weight=" + weight +
+                '}';
+    }
+}
